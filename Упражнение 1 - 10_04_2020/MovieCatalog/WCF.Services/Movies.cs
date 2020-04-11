5653415133574369e746e7b74b3104180655e4eb
Georgi Petrov <georgi.petrov96@abv.bg>
2020-04-11 21:10:21 +0300

    Initial project upload for the exercises done on 10.04.2020.

---
+++ Упражнение 1 - 10_04_2020/MovieCatalog/WCF.Services/Movies.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service2" in both code and config file together.
+    public class Movies : IMovies
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        public string Create(Movie movie)
+        {
+            Movie foundMovie = GetMovie(movie.Id);
+
+            if (foundMovie != null)
+            {
+                return "Movie id already used.";
+            }
+
+            movies.Add(movie);
+
+            return "Movie added successfully.";
+        }
+
+        public string Delete(int movieId)
+        {
+            Movie foundMovie = GetMovie(movieId);
+
+            if (foundMovie == null)
+            {
+                return "Movie not found.";
+            }
+
+            movies.Remove(foundMovie);
+
+            return "Movie removed successfully.";
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return movies;
+        }
+
+        public Movie GetMovie(int movieId)
+        {
+            Movie foundMovie = null;
+
+            foreach (var movie in movies)
+            {
+                if (movie.Id == movieId)
+                {
+                    foundMovie = movie;
+                }
+            }
+
+            return foundMovie;
+        }
+
+        public string Update(Movie movie)
+        {
+            Movie foundMovie = GetMovie(movie.Id);
+
+            if (foundMovie == null)
+            {
+                return "Movie not found.";
+            }
+
+            foundMovie.Rating = movie.Rating;
+            foundMovie.Title = movie.Title;
+            foundMovie.Genre = movie.Genre;
+
+            return "Update successful.";
+        }
+    }
+}
---
+++ Упражнение 1 - 10_04_2020/MovieCatalog/WCF.Services/Service1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        private static List<string> movieTitles = new List<string>();
+
+        public string Create(string movieTitle)
+        {
+            movieTitles.Add(movieTitle);
+
+            return "Movie title added successfully.";
+        }
+
+        public List<string> GetData()
+        {
+            return movieTitles;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
